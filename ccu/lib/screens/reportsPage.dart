22eab26fd39690135b16e107af07d78f46c61ab7
j-galveias <brainsuprajr@gmail.com>
2022-01-17 20:45:22 +0000

    Reports page

--- ccu/lib/screens/reportsPage.dart
+++ ccu/lib/screens/reportsPage.dart
@@ -173,102 +173,131 @@ class _ReportsPageState extends State<ReportsPage> {
                   ],
                 ),
               ),
-              Column(
-                children: <Widget>[
-                  Expanded(
-                    child: NotificationListener<ScrollNotification>(
-                      /*onNotification: (ScrollNotification scrollInfo) {
-                        if (!isLoading &&
-                            scrollInfo.metrics.pixels == scrollInfo.metrics.maxScrollExtent) {
-                              setState(() {
-                                isLoading = true;
-                              });
-                          db.getAllBalcs().then((value) {
-                            setState(() {
-                              if (value == null) {
-                                isLoading = false;
-                                return;
-                              }
-                              _balcs = value;
-                              isLoading = false;
-                            });
-                          });
-                        }
-                      },*/
-                      child: ListView.builder(
-                        itemCount: _balcs.length,
-                        itemBuilder: (context, index) {
-                          return Container(
-                            margin: EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 10.0),
-                            child: ClipRRect(
-                                borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                                child: InkWell(
-                                  onTap: () {
-                                    Navigator.pushNamed(context, '/showBalcao',
-                                        arguments: _balcs[index]);
-                                  },
-                                  child: Stack(
-                                    children: <Widget>[
-                                      Image.network(_balcs[index].thumbnail,
-                                          fit: BoxFit.cover, width: 1000.0),
-                                      Positioned(
-                                        bottom: 0.0,
-                                        left: 0.0,
-                                        right: 0.0,
-                                        child: Container(
-                                          decoration: BoxDecoration(
-                                            gradient: LinearGradient(
-                                              colors: [
-                                                Color.fromARGB(200, 0, 0, 0),
-                                                Color.fromARGB(0, 0, 0, 0)
-                                              ],
-                                              begin: Alignment.bottomCenter,
-                                              end: Alignment.topCenter,
-                                            ),
-                                          ),
-                                          padding: EdgeInsets.symmetric(
-                                              vertical: 10.0, horizontal: 10.0),
-                                          child: Row(
-                                              mainAxisAlignment:
-                                                  MainAxisAlignment.spaceBetween,
-                                              children: [
-                                                Text(
-                                                  '${_balcs[index].name}',
-                                                  style: TextStyle(
-                                                    color: Colors.white,
-                                                    fontSize: 24.0,
-                                                    fontWeight: FontWeight.bold,
-                                                  ),
-                                                ),
-                                                Text(
-                                                  '${_balcs[index].price}€',
-                                                  style: TextStyle(
-                                                    color: Colors.white,
-                                                    fontSize: 24.0,
-                                                    fontWeight: FontWeight.bold,
-                                                  ),
-                                                )
-                                              ]),
-                                        ),
-                                      ),
-                                    ],
-                                  ),
-                                )),
-                          );
-                        },
-                      ),
+              Flexible(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(0,10,0,0),
+                  child: ListView.builder(
+                    itemCount: 3,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: 200,
+                          decoration: BoxDecoration(
+                            color: Colors.blue[50],
+                            border: Border.all(
+                              color: Colors.blueAccent,
+                              width: 3,
+                            )
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Image(
+                                image: AssetImage('assets/logo.jpg'),
+                                width: 100,
+                                height: 180,
+                              ),
+                              Text("dd/mm/yy", 
+                                style: TextStyle(
+                                  color: Colors.blue,
+                                  fontSize: 20
+                                ),
+                              ),
+                              Text("To Be Reviewed", 
+                                style: TextStyle(
+                                  color: Colors.blue,
+                                  fontSize: 20
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+              /*Padding(
+                padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                          child: Text(
+                            "Daily Reports",
+                            style: TextStyle(
+                              color: Colors.blue,
+                              fontSize: 18.0)
+                          ),
+                        ),
+                        Text(
+                          "2/3",
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 18.0),
+                        ),
+                      ],
                     ),
-                  ),
-                  Container(
-                    height: isLoading ? 50.0 : 0,
-                    color: Colors.transparent,
-                    child: 
-                      Center(
-                        child: new CircularProgressIndicator(),
-                      ),
-                  ),
-                ],
-              )
+                    Container(
+                      height: 80, 
+                      child: 
+                        VerticalDivider(
+                          thickness: 1.8,
+                          color: Colors.blue[200])
+                    ),
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                          child: Text(
+                            "Approved",
+                            style: TextStyle(
+                              color: Colors.blue,
+                              fontSize: 18.0)
+                          ),
+                        ),
+                        Text(
+                          "4",
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 18.0),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      height: 80, 
+                      child: 
+                        VerticalDivider(
+                          thickness: 1.8,
+                          color: Colors.blue[200])
+                    ),
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                          child: Text(
+                            "Total Reports",
+                            style: TextStyle(
+                              color: Colors.blue,
+                              fontSize: 18.0)
+                          ),
+                        ),
+                        Text(
+                          "10",
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 18.0),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),*/
+              
            ]
           ),
         )
